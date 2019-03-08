8e49c4cbd15e1b5cf9298790272face29f110928
ShaMan123 <shacharnen@gmail.com>
2019-03-08 19:28:10 +0200

    update config

---
+++ babel.config.js
@@ -0,0 +1,24 @@
+﻿module.exports = function (api) {
+  api.cache(true)
+    return {
+        "presets": ["module:metro-react-native-babel-preset"],
+        "plugins": [
+            ["module-resolver", {
+                "root": ["./src"],
+                "extensions": [".js", ".ts", ".tsx", ".ios.js", ".android.js"]
+            }],
+            ["babel-plugin-inline-import", {
+                "extensions": [
+                    ".svg"
+                ]
+            }]
+        ],
+        "env": {
+            "production": {
+                "plugins": ["transform-remove-console"]
+            }
+        }
+    };
+}
+
+//  https://github.com/facebook/react-native/issues/21475#issuecomment-432509636

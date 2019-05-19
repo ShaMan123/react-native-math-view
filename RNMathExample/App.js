d2e736be45dafb6b88a7b2b6e5b22ca501582dfd
ShaMan123 <shacharnen@gmail.com>
2019-05-19 05:33:52 +0300

    Update App.js

--- RNMathExample/App.js
+++ RNMathExample/App.js
@@ -30,24 +30,27 @@ export default class App extends Component {
             state: 1,
             tag: MathStrings.calculus.filter((obj) => obj.math)[0],
             mip: false,
-            singleton: true
+            singleton: false
         }
         
     }
     
     componentDidMount() {
         let i = 0;
-        const interval = 4000;
+        const interval = 5000;
         const tags = MathStrings.calculus.filter((obj) => obj.math);
-        
-        this.t = setInterval(() => {
-            this.setState({
-                //width: Math.min(Dimensions.get('window').width * (i % 4 + 1) * 0.25, Dimensions.get('window').width),
-                tag: tags[i % tags.length],
-                mip: true
-            });
-            i++;
-        }, interval);
+
+        setTimeout(() => {
+            this.t = setInterval(() => {
+                this.setState({
+                    width: Math.min(Dimensions.get('window').width * (i % 4 + 1) * 0.25, Dimensions.get('window').width),
+                    tag: tags[i % tags.length],
+                    mip: true
+                });
+                i++;
+            }, interval);
+        }, interval)
+    
         
     }
 
@@ -57,97 +60,63 @@ export default class App extends Component {
 
     renderFlexItem(item) {
         const { string } = item;
-
         return (
             <MathView
                 math={string}
+                style={{maxWidth:this.state.width}}
             />
         );
     }
 
     renderItem(item) {
         return (
-            <View style={[styles.flexContainer, { flex: 1, backgroundColor: 'pink', margin: 5 }]}>
+            <View style={[styles.flexContainer, { flex: 1, backgroundColor: 'pink', /*alignItems:'center',justifyContent:'center'*/ }]}>
                 {this.renderFlexItem(item)}
             </View>
         );
     }
 
     render2() {
-        const data = _.flatten(this.state.sections.map(s => s.data));
-        return (
-            <View style={[{ flex: 1, maxWidth: this.state.width }]}>
-                <FlatList
-                    scrollEnabled
-                    renderItem={({ item, index, section }) => this.renderItem(item)}
-                    renderSectionHeader={({ section: { title } }) => <Text>{title}</Text>}
-                    data={this.state.singleton ? [data.shift()] : data}
-                    onRefresh={() => {
-                        this.setState({
-                            sections: [
-                                {
-                                    title: 'calculus',
-                                    data: this.state.sections[0].data.reverse(),
-                                    keyExtractor: (item) => `calculus:${item.string}`
-                                },
-                                {
-                                    title: 'trig',
-                                    data: this.state.sections[1].data.reverse(),
-                                    keyExtractor: (item) => `trig:${item.string}`
-                                }
-                            ]
-                        })
-                    }}
-                    keyExtractor={(item) => `math:flexWrap:${item.string}`}
-                    refreshing={this.state.refreshing}
-                    style={{ flex: 1 }}
-                    //contentContainerStyle={{ flex: 1 }}
-                    extraData={this.state.width}
-                    //CellRendererComponent={()=><View style={{ flex: 1, minWidth: 30, margin: 10, minHeight: 35, backgroundColor: 'purple' }} />}
-                    contentContainerStyle={/*this.state.mip && */[{flexWrap:'wrap', display:'flex',flexDirection:'row'}]}
-                />
-            </View>
-        );
+        return React.cloneElement(this.render1(), {
+            contentContainerStyle: { flexWrap: 'wrap', display: 'flex', flexDirection: 'row' }
+        });
     }
     
     render1() {
         return (
-            <View style={[{ flex: 1, maxWidth: this.state.width }]}>
-                <SectionList
-                    scrollEnabled
-                    renderItem={({ item, index, section }) => this.renderItem(item)}
-                    renderSectionHeader={({ section: { title } }) => <Text>{title}</Text>}
-                    sections={this.state.singleton? [
-                        {
-                            title: 'calculus',
-                            data: [this.state.sections[0].data[0]],
-                            keyExtractor: (item) => `calculus:${item.string}`
-                        }
-                    ] : this.state.sections}
-                    //sections={this.state.sections}
-                    onRefresh={() => {
-                        this.setState({
-                            sections: [
-                                {
-                                    title: 'calculus',
-                                    data: this.state.sections[0].data.reverse(),
-                                    keyExtractor: (item) => `calculus:${item.string}`
-                                },
-                                {
-                                    title: 'trig',
-                                    data: this.state.sections[1].data.reverse(),
-                                    keyExtractor: (item) => `trig:${item.string}`
-                                }
-                            ]
-                        })
-                    }}
-                    refreshing={this.state.refreshing}
-                    style={{ flex: 1 }}
-                    //contentContainerStyle={{flex:1}}
-                    extraData={this.state.width}
-                    //contentContainerStyle={this.state.mip && styles.flexContainer}
-                />
-            </View>
+            <SectionList
+                scrollEnabled
+                renderItem={({ item, index, section }) => this.renderItem(item)}
+                renderSectionHeader={({ section: { title } }) => <Text>{title}</Text>}
+                sections={this.state.singleton ? [
+                    {
+                        title: 'calculus',
+                        data: [this.state.sections[0].data[0]],
+                        keyExtractor: (item) => `calculus:${item.string}`
+                    }
+                ] : this.state.sections}
+                //sections={this.state.sections}
+                onRefresh={() => {
+                    this.setState({
+                        sections: [
+                            {
+                                title: 'calculus',
+                                data: this.state.sections[0].data.reverse(),
+                                keyExtractor: (item) => `calculus:${item.string}`
+                            },
+                            {
+                                title: 'trig',
+                                data: this.state.sections[1].data.reverse(),
+                                keyExtractor: (item) => `trig:${item.string}`
+                            }
+                        ]
+                    })
+                }}
+                refreshing={this.state.refreshing}
+                style={{ flex: 1, maxWidth: this.state.width }}
+                //contentContainerStyle={{flex:1}}
+                extraData={this.state.width}
+            />
         );
     }
 
@@ -156,41 +125,12 @@ export default class App extends Component {
     }
 
     render0() {
-        const data = [
-            {
-                containerStyle: [styles.mathContainer, styles.flexContainer,/* { flex: 1 }*/],
-                style: [/*styles.mathInner,*/ { /*flex: 1,*/ backgroundColor: 'blue' }]
-            },
-            {
-                containerStyle: [styles.mathContainer],
-                style: [/*styles.mathInner,*/ { /*flex: 1,*/ backgroundColor: 'blue' }]
-            },
-            {
-                containerStyle: [styles.mathContainer, styles.flexContainer,/* { flex: 1 }*/],
-                style: [/*styles.mathInner,*/ { flex: 1, backgroundColor: 'blue' }]
-            },
-            {
-                containerStyle: [styles.mathContainer, styles.flexContainer,/* { flex: 1 }*/],
-                style: [styles.mathInner, { flex: 1, backgroundColor: 'blue' }]
-            },
-            {
-                containerStyle: [styles.mathContainer, {minWidth: 200, minHeight: 150}],
-                style: [styles.mathInner, { flex: 1, backgroundColor: 'blue', minWidth: 200, minHeight:50 }]
-            }
-        ];
-
+        return this.renderItem(this.state.tag);
         return (
             <FlatList
                 scrollEnabled
-                renderItem={({ item, index }) => React.cloneElement(this.renderFlexItem(this.state.tag), item)}
-                data={this.state.singleton ? [data[0]] : data}
-                onRefresh={() => {
-                    this.setState((prevState) => {
-                        return {
-                            singleton: !prevState.singleton
-                        };
-                    });
-                }}
+                renderItem={({ item, index }) => this.renderFlexItem(this.state.tag)}
+                data={Array(5).fill(0)}
                 keyExtractor={(item, index) => `math:standalone:${index}`}
                 refreshing={this.state.refreshing}
                 style={{ flex: 1 }}

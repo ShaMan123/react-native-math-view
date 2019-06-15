bf99947004a03a2626669bb1e7903732ef99d9ac
ShaMan123 <shacharnen@gmail.com>
2019-06-15 16:42:52 +0300

    fix cache provider

--- src/android/MathJaxProvider.js
+++ src/android/MathJaxProvider.js
@@ -1,5 +1,4 @@
 'use strict';
-
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import {
@@ -19,6 +18,7 @@ import {
 } from 'react-native';
 import * as _ from 'lodash';
 import AsyncStorage from '@react-native-community/async-storage';
+import EventEmitter from 'events';
 
 const nativeViewName = 'RNMathJaxProvider';
 const RNMathJaxProvider = requireNativeComponent(nativeViewName, Provider, {
@@ -32,14 +32,16 @@ const ViewModule = Platform.OS === 'ios' ? ViewManager : NativeModules.RNMathVie
 
 let viewTag = null;
 const storageKey = 'MathJaxProviderCache';
-const result = [];
+const eventEmitter = new EventEmitter();
 
 async function getMathJaxNative(math) {
-    const response = await ViewModule.getMathJax(viewTag, math);
+    const request = Array.isArray(math) ? math : [math];
+    const response = await ViewModule.getMathJax(viewTag, request);
     _.map(response, (data) => {
         _getMathJax.cache.set(data.math, Promise.resolve(data));
     });
-    result.push(...response);
+    cacheHandler.setCache(response);
+    return response;
 }
 
 const _getMathJax = _.memoize(getMathJaxNative);
@@ -48,30 +50,27 @@ export async function getMathJax(math) {
     const isArr = Array.isArray(math);
     const arr = isArr ? math : [math];
     const fetchFromNative = _.filter(arr, (str) => !_getMathJax.cache.has(str));
-    if (_.size(fetchFromNative) > 0) await getMathJaxNative(fetchFromNative);
+    if (_.size(fetchFromNative) > 0) {
+        const canResolve = () => !_.some(fetchFromNative, (str) => !_getMathJax.cache.has(str));
+        await Promise.race([
+            new Promise((resolve) => {
+                if (cacheHandler.active && canResolve) resolve();
+                eventEmitter.once('active', () => canResolve() && resolve());
+            }),
+            getMathJaxNative(fetchFromNative)
+        ]);
+    }
+    
     const response = await Promise.all(_.map(arr, _getMathJax));
     return isArr ? response : response[0];
 }
 
 class CacheHandler {
+    active = false;
+    cache = [];
     constructor() {
         this.appState = AppState.currentState;
-        AppState.addEventListener('change', this.listener);
         this.getCache();
-    }
-
-    listener = (nextAppState) => {
-        if (nextAppState.match(/inactive|background/)) {
-            this.setCache();
-        }
-        else {
-            this.getCache();
-        }
-        this.appState = nextAppState;
-    }
-
-    destroy() {
-        AppState.removeEventListener('change', this.listener);
     }
 
     async getCache() {
@@ -79,21 +78,42 @@ class CacheHandler {
             const response = await AsyncStorage.getItem(storageKey);
             if (!response) return;
             _.map(JSON.parse(response), (data) => {
+                this.cache.push(data);
                 _getMathJax.cache.set(data.math, Promise.resolve(data));
             });
+            if (!this.active) {
+                this.active = true;
+                eventEmitter.emit('active');
+            }            
         }
     }
 
-    setCache() {
-        if (AsyncStorage) AsyncStorage.setItem(storageKey, JSON.stringify(result));
+    async setCache(data) {
+        if (AsyncStorage) {
+            this.cache.push(...data);
+            return AsyncStorage.setItem(storageKey, JSON.stringify(this.cache));
+        }
     }
 }
 
 const cacheHandler = new CacheHandler();
 
 export class Provider extends Component {
+    static propTypes = {
+        preload: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.string), PropTypes.string])
+    }
+
+    static getDerivedStateFormProps(nextProps) {
+        getMathJax(nextProps.preload);
+        return null;
+    }
+
     _handleRef = (ref) => {
         viewTag = ref ? findNodeHandle(ref) : null;
+    }
+
+    preload(math) {
+        return getMathJax(math);
     }
 
     render() {

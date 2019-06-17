52a5191c765722d368137af49b7f14b67fd3cfa1
ShaMan123 <shacharnen@gmail.com>
2019-06-17 12:21:43 +0300

    fix stubs

--- src/ios/MathProvider.js
+++ src/ios/MathProvider.js
@@ -2,11 +2,25 @@
 import React from 'react';
 
 export class CacheHandler {
-
+    warnings = true;
+    stub = () => { this.warnings && console.warn('MathProvider: stubbed `CacheManager` method'); }
+    getCache = this.stub;
+    addToCache = this.stub;
+    clearDatabase = this.stub;
+    clearCache = this.stub;
+    isCached = this.stub;
+    enable = this.stub;
+    disable = this.stub;
+    disableWarnings() {
+        this.warnings = false;
+    }
+    setMaxTimeout = this.stub;
+    handleRequest = this.stub;
+    fetch = this.stub;
 }
 
 export const CacheManager = new CacheHandler();
 
 export function Provider(props) {
-    return this.props.children;
+    return props.children;
 }
--- src/ios/MathProviderHOC.js
+++ src/ios/MathProviderHOC.js
@@ -1,6 +1,6 @@
 'use strict';
 import React from 'react';
 
-export default function MathProviderHOC(WrappedComponent, preloadMath) {
+export default function MathProviderHOC(WrappedComponent, props) {
     return WrappedComponent;
 }

863cae9c1bb7b88e4bde35dd3c09146362d28883
ShaMan123 <shacharnen@gmail.com>
2019-07-11 09:41:27 +0300

    Update RNMathViewPackage.java

--- android/src/main/java/io/autodidact/rnmathview/RNMathViewPackage.java
+++ android/src/main/java/io/autodidact/rnmathview/RNMathViewPackage.java
@@ -18,8 +18,7 @@ public class RNMathViewPackage implements ReactPackage {
     }
 
     @Override
-    public List<NativeModule> createNativeModules(
-            ReactApplicationContext reactContext) {
+    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
         List<NativeModule> modules = new ArrayList<>();
 
         modules.add(new RNMathViewModule(reactContext));

1aa6d013b3736a98fe354675418f1896e921c67b
ShaMan123 <shacharnen@gmail.com>
2019-07-11 09:41:17 +0300

    example app RN0.60

---
+++ MathExample/react-native.config.js
@@ -0,0 +1,10 @@
+﻿//  https://github.com/react-native-community/cli/blob/5199d6af1aa6dc5d8dfb4a98e675987272d68998/docs/configuration.md
+
+module.exports = {
+    // config for a library is scoped under "dependency" key
+    dependencies: {
+        'react-native-math-view': {
+            root: '..'
+        }
+    },
+};

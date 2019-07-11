1d73a98bbaf20b8312cf52b31e5cf364abb51d22
ShaMan123 <shacharnen@gmail.com>
2019-07-11 09:21:31 +0300

    RN0.60

---
+++ react-native.config.js
@@ -0,0 +1,22 @@
+﻿
+//  https://github.com/react-native-community/cli/blob/5199d6af1aa6dc5d8dfb4a98e675987272d68998/docs/configuration.md
+
+module.exports = {
+    // config for a library is scoped under "dependency" key
+    dependency: {
+        platforms: {
+            //ios: {},
+            android: {
+                packageImportPath: 'import com.autodidact.rnmathview'
+            }, // projects are grouped into "platforms"
+        },
+        assets: [
+            "MathJaxProvider/index.html",
+            "MathJaxProvider/dist"
+        ],
+        // hooks are considered anti-pattern, please avoid them
+        hooks: {
+            //prelink: './path-to-a-prelink-hook',
+        },
+    },
+};

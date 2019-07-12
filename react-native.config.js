59057ea7ab8ed973935667eeb67802cc8e7de727
ShaMan123 <shacharnen@gmail.com>
2019-07-12 09:38:45 +0300

    Update react-native.config.js

--- react-native.config.js
+++ react-native.config.js
@@ -1,23 +1,20 @@
-﻿
-//  https://github.com/react-native-community/cli/blob/5199d6af1aa6dc5d8dfb4a98e675987272d68998/docs/configuration.md
+﻿//  https://github.com/react-native-community/cli/blob/5199d6af1aa6dc5d8dfb4a98e675987272d68998/docs/configuration.md
 
-const path = require('path');
-const root = __dirname; //path.resolve(process.cwd(), 'node_modules/react-native-math-view');
+//const path = require('path');
+//const root = __dirname;
 
 module.exports = {
-    // config for a library is scoped under "dependency" key
     dependency: {
         platforms: {
             android: {
-                sourceDir: path.resolve(root, 'android'),
-                //folder: root,
                 packageImportPath: "import io.autodidact.rnmathview.RNMathViewPackage;",
                 packageInstance: "new RNMathViewPackage()"
             },
+            ios: {}
         },
         assets: [
-            "MathJaxProvider/index.html",
-            "MathJaxProvider/dist"
+            //"MathJaxProvider/index.html"
+            //"MathJaxProvider/dist"
         ],
     },
 };

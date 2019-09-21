db81adefe8b04217a9514f40536c185fd10628f8
ShaMan123 <shacharnen@gmail.com>
2019-09-22 00:56:43 +0300

    Update react-native-math-view.d.ts

--- react-native-math-view.d.ts
+++ react-native-math-view.d.ts
@@ -1,6 +1,17 @@
+import { ViewStyle } from 'react-native';
 import MathViewAndroid, { MathViewProps as MathViewPropsAndroid } from './src/android/MathView';
-import MathViewIOS, { MathViewProps as MathViewPropsIOS } from './src/ios/MathView';
+import { CalculatedStyleConfig } from './src/CalculatedStyle';
+import { MathToSVGConfig } from './src/Config';
+import { MathViewProps as MathViewPropsIOS } from './src/ios/MathView';
+import { MathProviderResponse } from './src/MathProvider';
+
+
 declare module 'react-native-math-view' {
     export type MathViewProps = MathViewPropsAndroid | MathViewPropsIOS;
-    export default function MathView(props: MathViewProps): ReturnType<typeof MathViewAndroid>
+    export default function MathView(props: MathViewProps): ReturnType<typeof MathViewAndroid>;
+    export function useCalculatedStyle(mathOrResponse: string | MathProviderResponse, config: Partial<CalculatedStyleConfig>): ViewStyle;
+    
+    export module MathProvider {
+        export function mathToSvg(math: string, config?: Partial<MathToSVGConfig>): MathProviderResponse
+    }
 }

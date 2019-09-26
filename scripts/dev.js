85ba3bef0917d24ce54e4f05e58f2f0174b62f2c
ShaMan123 <shacharnen@gmail.com>
2019-09-26 21:11:13 +0300

    Update dev.js

--- scripts/dev.js
+++ scripts/dev.js
@@ -1,33 +1,44 @@
-﻿const child_process = require('child_process');
+﻿#!/usr/bin / env node
+const child_process = require('child_process');
 const path = require('path');
 const chalk = require('chalk');
 const fs = require('fs-extra');
 const _ = require('lodash');
-//const yargs = require('yargs');
+const argv = require('yargs').argv;
 
 const processConfig = {
     cwd: path.resolve(__dirname, '..')
 };
 
-const start = 'npm start';
-const tsc = 'tsc --watch';
-
-fs.emptyDirSync(path.resolve(processConfig.cwd, 'dist'));
-runCommand(tsc);
-runCommand(start, _.assign(processConfig, { detached: false, stdio: 'inherit' }));
-
 function runCommand(command, procConfig = processConfig) {
     if (process.platform === 'darwin') {
-        child_process.spawnSync('open', [command], procConfig);
+        return child_process.spawnSync('open', [command], procConfig);
 
     } else if (process.platform === 'linux') {
-        child_process.spawn('sh', [command], _.defaultsDeep(procConfig, { detached: true }));
+        return child_process.spawn('sh', [command], _.defaultsDeep(procConfig, { detached: true }));
 
     } else if (/^win/.test(process.platform)) {
-        child_process.spawn('cmd.exe', ['/C', command], _.defaultsDeep(procConfig, { detached: true, stdio: 'ignore' }));
-
+        return child_process.spawn('cmd.exe', ['/C', command], _.defaultsDeep(procConfig, { detached: true, stdio: 'ignore' }));
     } else {
         console.log(chalk.red(`Cannot start the packager. Unknown platform ${process.platform}`));
     }
 }
 
+function tsc() {
+    const cmd = 'tsc --watch';
+    fs.emptyDirSync(path.resolve(processConfig.cwd, 'dist'));
+    return runCommand(cmd);
+}
+
+function start() {
+    const cmd = 'npm start';
+    runCommand(cmd, _.assign(processConfig, { detached: false, stdio: 'inherit' }));
+}
+
+function dev() {
+    tsc();
+    setTimeout(start, 1000);
+}
+
+if (argv.tsc) tsc();
+else dev();

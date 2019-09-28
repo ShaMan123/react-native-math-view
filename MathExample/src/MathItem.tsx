19a8569ccc34b075799158bed26a163639f0506d
ShaMan123 <shacharnen@gmail.com>
2019-09-28 20:42:15 +0300

    Update MathItem.tsx

--- MathExample/src/MathItem.tsx
+++ MathExample/src/MathItem.tsx
@@ -1,13 +1,12 @@
 
-import React, { useEffect, useMemo } from 'react';
-import { Animated, TouchableOpacity, TouchableOpacityProps, ViewProps } from 'react-native';
+import React, { useEffect, useMemo, forwardRef } from 'react';
+import { Animated, TouchableOpacityProps, ViewProps } from 'react-native';
 import MathView, { MathViewProps } from 'react-native-math-view/src';
 import { useColor } from './Hooks';
 import styles from './styles';
+import { TouchableHighlight, TouchableOpacity } from 'react-native-gesture-handler';
 
-const AnimatedTouchable = Animated.createAnimatedComponent(TouchableOpacity);
-
-function MathItem(props: MathViewProps & TouchableOpacityProps & { containerStyle?: ViewProps['style'] }) {
+function MathItem(props: MathViewProps & TouchableOpacityProps & { containerStyle?: ViewProps['style'] }, ref: any) {
     const color = useColor();
 
     const opacity = useMemo(() => new Animated.Value(0), []);
@@ -21,22 +20,26 @@ function MathItem(props: MathViewProps & TouchableOpacityProps & { containerStyl
     }, []);
     
     return (
-        <AnimatedTouchable
+        <TouchableOpacity
             {...props}
             style={[props.containerStyle, /*{ opacity }*/]}
+            //activeOpacity={0.2}
         >
             <MathView
                 //color={parsedColor()} // can set color in style or directly
                 resizeMode='contain'
                 style={{ color }} // can use color prop instead
                 {...props}
+                ref={ref}
             />
-        </AnimatedTouchable>
+        </TouchableOpacity>
     );
 }
 
-MathItem.defaultProps = {
+const MathItemWrapper = forwardRef(MathItem);
+
+MathItemWrapper.defaultProps = {
     containerStyle: styles.flexContainer
 }
 
-export default MathItem;
+export default MathItemWrapper;

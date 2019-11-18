9be10fcdd2fb37f48714dc1c33876139b8bfe035
ShaMan123 <shacharnen@gmail.com>
2019-11-18 14:03:47 +0200

    Update compileMathjax.js

--- scripts/compileMathjax.js
+++ scripts/compileMathjax.js
@@ -1,24 +1,30 @@
 ﻿const child_process = require('child_process');
 const path = require('path');
 const chalk = require('chalk');
-const fs = require('fs');
+const fs = require('fs-extra');
+
+
+const mathjaxEntry = require.resolve('mathjax-full');
+const mathjaxDir = mathjaxEntry.substring(0, mathjaxEntry.indexOf('mathjax-full') + 'mathjax-full'.length);
 
 const dir = path.resolve(__dirname, '..');
 const cwd = dir.substring(0, dir.indexOf('node_modules'));  // dir.includes('node_modules') ? path.resolve(dir, '..', '..') : dir
 
 const processConfig = {
     stdio: 'inherit',
-    cwd
+    cwd: mathjaxDir,
+    //detached: true
 };
 
 const innerPathToFile = 'input/tex/mhchem/mhchem_parser.js';
-const pathToModule = 'node_modules/mathjax-full';
 
 try {
-    child_process.execSync('cd node_modules/mathjax-full && npm run compile', processConfig);
+    //  compiles ts files
+    console.log(chalk.bold('compiling mathjax'));
+    child_process.execSync('npm run compile', processConfig);
 }
 catch (e) { };
 //child_process.execSync('cd node_modules/mathjax-full && npm run postcompile', processConfig);
-fs.copyFileSync(path.resolve(processConfig.cwd, pathToModule, 'ts', innerPathToFile), path.resolve(processConfig.cwd, pathToModule, 'js', innerPathToFile));
+fs.copyFileSync(path.resolve(processConfig.cwd, 'ts', innerPathToFile), path.resolve(processConfig.cwd, 'js', innerPathToFile));
 
-console.log(chalk.bold('compiled mathjax successfully'));
+console.log(chalk.bold('\n\n\ncompiled mathjax successfully'));

9b56b76911278469917b5b6ca239e7094dae5759
ShaMan123 <shacharnen@gmail.com>
2020-04-03 17:24:01 +0300

    Update Config.ts

--- src/mathjax/Config.ts
+++ src/mathjax/Config.ts
@@ -2,62 +2,70 @@
 import { AllPackages } from 'mathjax-full/js/input/tex/AllPackages';
 
 export interface MathToSVGConfig {
-    /** 
-     *  process as inline math
-     *  default: true
-     *  */
-    inline: boolean,
+	/** 
+	 *  process as inline math
+	 *  default: true
+	 *  */
+	inline: boolean,
 
-    /**
-     * em-size in pixels
-     * default: 16 
-     * */
-    em: number,
+	/**
+	 * em-size in pixels
+	 * default: 16 
+	 * */
+	em: number,
 
-    /**
-     * ex-size in pixels
-     * default: 8 
-     * */
-    ex: number,
+	/**
+	 * ex-size in pixels
+	 * default: 8 
+	 * */
+	ex: number,
 
-    /**
-     * width of container in pixels
-     * default: 80 * 16
-     * */
-    width: number,
+	/**
+	 * width of container in pixels
+	 * default: 80 * 16
+	 * */
+	width: number,
 
-    /**
-      * the packages to use, e.g. "base, ams"
-      * default: AllPackages
-      * */
-    packages: string[],
+	/**
+	  * the packages to use, e.g. "base, ams"
+	  * default: AllPackages
+	  * */
+	packages: string[],
 
-    /**
-     * output the required CSS rather than the SVG itself
-     * default: false 
-     * won't be used in react-native context
-     * */
-    //css: boolean,
+	/**
+	 * output the required CSS rather than the SVG itself
+	 * default: false 
+	 * won't be used in react-native context
+	 * */
+	//css: boolean,
 
-    /**
-     * whether to use a local font cache or not
-     * default: true 
-     * */
-    fontCache: boolean,
+	/**
+	 * whether to use a local font cache or not
+	 * default: true 
+	 * */
+	fontCache: boolean,
 
-    /**
-     * https://docs.mathjax.org/en/latest/options/accessibility.html#semantic-enrich-options
-     * */
-    enrichSpeech: 'none' | 'shallow' | 'deep',
+	/**
+	 * https://docs.mathjax.org/en/latest/options/accessibility.html#semantic-enrich-options
+	 * */
+	enrichSpeech: 'none' | 'shallow' | 'deep',
+
+	/**
+	 * https://docs.mathjax.org/en/v1.0/options/tex2jax.html#configure-tex2jax
+	 */
+	inlineMath: string[][],
+	displayMath: string[][]
 }
 
 export const mathToSVGDefaultConfig = {
-    inline: true,
-    em: 16,
-    ex: 8,
-    width: 80 * 16,
-    packages: AllPackages,
-    //css: false,
-    fontCache: true,
-    enrichSpeech: 'deep'
+	inline: true,
+	em: 16,
+	ex: 8,
+	width: 80 * 16,
+	packages: AllPackages,
+	//css: false,
+	fontCache: true,
+	enrichSpeech: 'deep',
+	inlineMath: [['$', '$'], ['\\(', '\\)']],
+	displayMath: [['$$', '$$'], ['\[', '\]'], ['\\text{', '}']]
 } as MathToSVGConfig;

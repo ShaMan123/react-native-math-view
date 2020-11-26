f0a991b24620e123c06aa841505ba66df6a72039
ShaMan123 <shacharnen@gmail.com>
2020-11-26 23:43:48 +0200

    types

--- src/index.ios.ts
+++ src/index.ios.ts
@@ -4,3 +4,4 @@ export const Constants = {};
 //  fallback to SvgXml
 //export { default } from './ios/MathView';
 export { default } from './fallback';
+export type { MathViewProps } from './common';
--- src/ios/MathView.tsx
+++ src/ios/MathView.tsx
@@ -1,4 +1,5 @@
 'use strict';
+import { MathViewProps } from 'common';
 import * as React from 'react';
 import { NativeModules, requireNativeComponent, UIManager, ViewProps } from 'react-native';
 
@@ -7,24 +8,13 @@ const RNMathView = requireNativeComponent(nativeViewName);
 const MathViewManager = NativeModules.RNMathViewManager || {};
 export const { Constants } = UIManager.getViewManagerConfig(nativeViewName) || {};
 
-export interface MathViewProps extends ViewProps {
-    math: string,
-
-    /**
-     * set text color
-     * can be set via `setNativeProps` or passed via `style`
-     * */
-    color?: string,
-    style?: ViewProps['style'] & { color: any }
-}
-
-function MathView(props: MathViewProps, ref: any) {
+const MathView = React.forwardRef((props: MathViewProps, ref: any) => {
     return (
         <RNMathView
             {...props}
             ref={ref}
         />
     );
-}
+});
 
-export default React.forwardRef(MathView);
+export default MathView;

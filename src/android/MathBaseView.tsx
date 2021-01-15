6d469e05494fa5af32191022aa4b70326a6b767f
ShaMan123 <shacharnen@gmail.com>
2021-01-15 12:50:50 +0200

    Update MathBaseView.tsx

--- src/android/MathBaseView.tsx
+++ src/android/MathBaseView.tsx
@@ -1,15 +1,16 @@
 'use strict';
 
 import _ from 'lodash';
-import React, { forwardRef, Ref, useEffect, useMemo, useState } from 'react';
+import React, { forwardRef, Ref, useMemo } from 'react';
 import { NativeModules, requireNativeComponent, UIManager } from 'react-native';
-import { ErrorComponent, MathViewProps, styles } from '../common';
+import { MathViewProps, styles } from '../common';
 import MathjaxFactory from '../mathjax';
 
 const nativeViewName = 'RNMathView';
 const RNMathView = requireNativeComponent(nativeViewName);
 const MathViewManager = NativeModules.RNMathViewManager || {};
-//console.log(UIManager.getViewManagerConfig('getConstants')().RNMathView)
+
+export type TConstants = { "PreserveAspectRatio": { "Alignment": { "none": "none", "xMaxYMax": "xMaxYMax", "xMaxYMid": "xMaxYMid", "xMaxYMin": "xMaxYMin", "xMidYMax": "xMidYMax", "xMidYMid": "xMidYMid", "xMidYMin": "xMidYMin", "xMinYMax": "xMinYMax", "xMinYMid": "xMinYMid", "xMinYMin": "xMinYMin" }, "BOTTOM": "xMidYMax meet", "END": "xMaxYMax meet", "FULLSCREEN": "xMidYMid slice", "FULLSCREEN_START": "xMinYMin slice", "LETTERBOX": "xMidYMid meet", "START": "xMinYMin meet", "STRETCH": "none null", "Scale": { "meet": "meet", "slice": "slice" }, "TOP": "xMidYMin meet", "UNSCALED": "null null" }, "ScaleType": { "CENTER": "CENTER", "CENTER_CROP": "CENTER_CROP", "CENTER_INSIDE": "CENTER_INSIDE", "FIT_CENTER": "FIT_CENTER", "FIT_END": "FIT_END", "FIT_START": "FIT_START", "FIT_XY": "FIT_XY", "MATRIX": "MATRIX" } };
 export const { Constants } = UIManager.getViewManagerConfig(nativeViewName) || {};
 
 export interface MathViewBaseProps extends MathViewProps {
@@ -46,8 +47,8 @@ const MathBaseView = forwardRef((props: MathViewBaseProps, ref: Ref<any>) => {
 
 MathBaseView.defaultProps = {
     resizeMode: 'contain',
+    preserveAspectRatio: "xMinYMid meet",
     config: {}
 } as Partial<MathViewBaseProps>;
 
 export { MathBaseView as default };
-

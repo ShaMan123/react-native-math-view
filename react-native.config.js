8d7ebb2f9fe2b034f52a52b0d9b0557f9a4ec671
Stefano Redaelli <rrr3da@hotmail.com>
2023-01-04 16:08:58 +0100

    removed assets in config

--- react-native.config.js
+++ react-native.config.js
@@ -12,9 +12,5 @@ module.exports = {
             },
             ios: {}
         },
-        assets: [
-            //"MathJaxProvider/index.html"
-            //"MathJaxProvider/dist"
-        ],
     },
 };
